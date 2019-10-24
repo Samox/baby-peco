04c0888c70fedda776c40f40a73fb61eefd03eba
Sammy Teillet <sammy.teillet@gmail.com>
2019-10-24 23:19:37 +0200

    Updates

---
+++ precache-manifest.4a397a13e1ee83c1ac4ab6be17a5b8fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fae93019cf6e3439a9824d57bdb77a6",
+    "url": "/baby-peco/index.html"
+  },
+  {
+    "revision": "d500326177e390bf7fca",
+    "url": "/baby-peco/static/css/main.b32a2121.chunk.css"
+  },
+  {
+    "revision": "d792a90c683344d6c600",
+    "url": "/baby-peco/static/js/2.70b29706.chunk.js"
+  },
+  {
+    "revision": "d500326177e390bf7fca",
+    "url": "/baby-peco/static/js/main.8d7dd1bf.chunk.js"
+  },
+  {
+    "revision": "6cc6897e2756fa71326a",
+    "url": "/baby-peco/static/js/runtime-main.36085534.js"
+  }
+]);
